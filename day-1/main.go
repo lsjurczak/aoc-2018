35ffce2a089f462a3a1c17fc1a21ab51c740192f
Łukasz Jurczak <lsjurczak@gmail.com>
2018-12-02 19:41:41 +0100

    Add day 1 optimization

--- day-1/main.go
+++ day-1/main.go
@@ -8,27 +8,19 @@ import (
 	"strings"
 )
 
-func partOne(fileData []byte) int {
+func partOne(data []int) int {
 	var sum int
-	for _, line := range strings.Split(string(fileData), "\n") {
-		val, err := strconv.Atoi(line)
-		if err != nil {
-			log.Fatal(err)
-		}
+	for _, val := range data {
 		sum += val
 	}
 	return sum
 }
 
-func partTwo(fileData []byte) int {
+func partTwo(data []int) int {
 	var freq int
 	seen := make(map[int]bool)
 	for {
-		for _, line := range strings.Split(string(fileData), "\n") {
-			val, err := strconv.Atoi(line)
-			if err != nil {
-				log.Fatal(err)
-			}
+		for _, val := range data {
 			freq += val
 			if _, ok := seen[freq]; ok {
 				return freq
@@ -44,6 +36,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("1)", partOne(file))
-	fmt.Println("2)", partTwo(file))
+	data := make([]int, 0, 256)
+	for _, line := range strings.Split(string(file), "\n") {
+		val, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = append(data, val)
+	}
+
+	fmt.Println("1)", partOne(data))
+	fmt.Println("2)", partTwo(data))
 }
